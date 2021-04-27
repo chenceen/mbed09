676f9d7f8ce449794cd08c19ead3d18095ca01e9
chenceen <ccs19991208@gmail.com>
2021-04-27 10:19:32 -0700

    add demo

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,25 +1,48 @@
-#include "mbed.h"
+  #include "mbed.h"
 #include "mbed_rpc.h"
 
-RpcDigitalOut myled2(LED2,"myled2");
-RpcDigitalOut myled3(LED3,"myled3");
+DigitalOut myled2(LED2);
+DigitalOut myled3(LED3);
 BufferedSerial pc(USBTX, USBRX);
 void LEDControl(Arguments *in, Reply *out);
 RPCFunction rpcLED(&LEDControl, "LEDControl");
-double x, y;
+double x;
+Thread thread;
 
-int main() {
+void blink() 
+{
+    while(1) 
+    {
+        if(x==1)
+        {
+            myled2 = !myled2;
+            ThisThread::sleep_for(250ms);
+            myled3 = !myled3;
+            ThisThread::sleep_for(250ms);
+        }
+        else
+        {
+            myled2 = 0;
+            myled3 = 1;
+        }
+    }
+}
 
+int main() 
+{
     char buf[256], outbuf[256];
-
     FILE *devin = fdopen(&pc, "r");
     FILE *devout = fdopen(&pc, "w");
+    thread.start(blink);
 
-    while(1) {
+    while(1) 
+    {
         memset(buf, 0, 256);
-        for (int i = 0; ; i++) {
+        for (int i = 0; ; i++) 
+        {
             char recv = fgetc(devin);
-            if (recv == '\n') {
+            if (recv == '\n') 
+            {
                 printf("\r\n");
                 break;
             }
@@ -28,48 +51,21 @@ int main() {
         RPC::call(buf, outbuf);
         printf("%s\r\n", outbuf);
     }
+
 }
 
-
-void LEDControl (Arguments *in, Reply *out)   {
-    bool success = true;    
-    x = in->getArg<double>();
-    
+void LEDControl (Arguments *in, Reply *out)   
+{
+    bool success = true;
     char buffer[200], outbuf[256];
     char strings[20];
+    x = in->getArg<double>();
+    if(x==1) sprintf(strings, "LED blinking");
+    else sprintf(strings, "STOP");
+    strcpy(buffer, strings);
+    RPC::call(buffer, outbuf);
+    if (success) out->putData(buffer);
+    else out->putData("Failed to execute LED control.");
 
-    if(x==1){
-        while(1){
-        sprintf(strings, "/myled2/write 1");
-        strcpy(buffer, strings);
-        RPC::call(buffer, outbuf);
-        out->putData(buffer);
-        ThisThread::sleep_for(500ms);
-        sprintf(strings, "/myled2/write 0");
-        strcpy(buffer, strings);
-        RPC::call(buffer, outbuf);
-        out->putData(buffer);
-        ThisThread::sleep_for(500ms);
-        sprintf(strings, "/myled3/write 0");
-        strcpy(buffer, strings);
-        RPC::call(buffer, outbuf);
-        out->putData(buffer);
-        ThisThread::sleep_for(500ms);
-        sprintf(strings, "/myled3/write 1");
-        strcpy(buffer, strings);
-        RPC::call(buffer, outbuf);
-        out->putData(buffer);
-        ThisThread::sleep_for(500ms);
-        }
-    }
-    else {
-        sprintf(strings, "/myled2/write 0");
-        strcpy(buffer, strings);
-        RPC::call(buffer, outbuf);
-        out->putData(buffer);
-        sprintf(strings, "/myled3/write 1");
-        strcpy(buffer, strings);
-        RPC::call(buffer, outbuf);
-        out->putData(buffer);    
-    }
 }
+
